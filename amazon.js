ab362df01f6e573b739efc6db169252bd0a1814e
jimpo26 <76153531+jimpo26@users.noreply.github.com>
2021-06-07 10:43:07 +0200

    Add files via upload

---
+++ amazon.js
@@ -0,0 +1,28 @@
+function cartBtn() 
+{
+    const item = document.getElementById("#ppd");
+    if ( item == null)
+    {
+        const err = 'You have to run this script on Amazon.\nThen the add to cart button will appear.';
+        alert(err);
+        throw new Error(err);
+    }
+    else
+    {
+        if(document.getElementByClassName("a-button-input attach-dss-atc").lenght == 0)
+        {
+            if(document.getElementByClassName("a-section a-spacing-small a-text-center").lenght > 0)
+            {
+                document.getElementByClassName("product-out-of-stock")[0].remove();
+                var btn = document.createElement("button");
+                btn.setAttribute("class","a-button-input attach-dss-atc");
+                btn.setAttribute("href","/gp/product/handle-buy-box/ref=dp_start-bbf_1_glance");
+                btn.setAttribute("style", "color: #353DD4 !important;background-color: ##599EE3 !important;border: 2px solid #353DD4 !important;");
+                btn.innerHTML = "Electratech | Cart";
+                document.getElementByClassName("a-box-inner")[0].appendChild(button)
+            }
+        }
+    }
+    Drupal.ajax.bindAjaxLinks(document);
+    return item;
+}
