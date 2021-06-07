922a5baf25e0a2a6ddb40f6cee81fc264a876b7e
jimpo26 <76153531+jimpo26@users.noreply.github.com>
2021-06-07 11:17:19 +0200

    Update amazon.js

--- amazon.js
+++ amazon.js
@@ -3,16 +3,20 @@ function cartBtn()
     const item = document.getElementById("#ppd");
     if ( item == null)
     {
+        console.log("Item è null")
         const err = 'You have to run this script on Amazon.\nThen the add to cart button will appear.';
         alert(err);
         throw new Error(err);
     }
     else
     {
+        console.log("sono nell'else")
         if(document.getElementByClassName("a-button-input attach-dss-atc").lenght == 0)
         {
+            console.log("if 1")
             if(document.getElementByClassName("a-section a-spacing-small a-text-center").lenght > 0)
             {
+                console.log("if 2")
                 document.getElementByClassName("product-out-of-stock")[0].remove();
                 var btn = document.createElement("button");
                 btn.setAttribute("class","a-button-input attach-dss-atc");
