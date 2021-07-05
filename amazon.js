2ab676e25ede8313caad7343a072138e85275264
jimpo26 <76153531+jimpo26@users.noreply.github.com>
2021-07-05 17:45:26 +0200

    Update amazon.js

--- amazon.js
+++ amazon.js
@@ -22,5 +22,4 @@ function btn(){
 }
 btn()
 
-//use this to run on bookmarks
-javascript:(function()%7Bconst%20item%20%3D%20document.getElementsByClassName(%22a-box%22)%5B0%5D%3B%3Bif%20(%20item%20%3D%3D%20null)%7B%3Bconst%20err%20%3D%20'You%20have%20to%20run%20this%20script%20on%20Amazon.%5CnThen%20the%20add%20to%20cart%20button%20will%20appear.'%3Balert(err)%3Bthrow%20new%20Error(err)%3B%7Delse%7B%3Bif(document.getElementsByClassName(%22a-button-input%20attach-dss-atc%22).length%20%3D%3D%200)%7B%3Bif(document.getElementsByClassName(%22a-color-price%20a-text-bold%22).length%20%3E%200)%7B%3Bvar%20out=document.getElementsByClassName(%22a-color-price%20a-text-bold%22)[0].innerHTML = "Electratech%20|%20Cart<br>Click%20here%20to%20add%20the%20item%20to%20the%20cart";document.getElementsByClassName("a-color-price%20a-text-bold")[0].setAttribute("style","color:%20#3d35df%20!important;");%3Bvar%20btn%20%3D%20document.createElement(%22button%22)%3Bbtn.setAttribute(%22class%22%2C%22a-button-input%20attach-dss-atc%22)%3Bbtn.setAttribute(%22href%22%2C%20%22#addToCart%22)%3Bdocument.getElementsByClassName(%22a-section%20a-spacing-small%20a-text-center%22)%5B0%5D.appendChild(btn)%3B%7D%7D%7D%7D)()
+//to run this script on bookmarks, copy the script from pastebin https://pastebin.com/raw/SNVHBhEg and paste it in the bookmark
