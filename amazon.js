1722461f84d7e3020bffa39cfbac4ac62c6ad79b
jimpo26 <76153531+jimpo26@users.noreply.github.com>
2023-06-05 19:10:36 +0200

    Update amazon.js

--- amazon.js
+++ amazon.js
@@ -1,26 +1,17 @@
-// use this to run with fetch (in raw obviously)
-//https://raw.githubusercontent.com/jimpo26/amazon/main/amazon.js
 function btn(){
-    const item = document.getElementsByClassName("a-box")[0];
+    const item = document.getElementById("outOfStock");
     if (item === null){
         const err = "You have to run this script on Amazon's not available item page.\nThen the add to cart button will appear.";
         alert(err);
         throw new Error(err);
     }
     else{
-        if(document.getElementsByClassName("a-button-input attach-dss-atc").length === 0){
-            if(document.getElementsByClassName("a-color-price a-text-bold").length > 0){
-                document.getElementsByClassName("a-color-price a-text-bold")[0].innerHTML = "Jimpo26 | Cart<br>Click here to add the item to the cart";
-                document.getElementsByClassName("a-color-price a-text-bold")[0].setAttribute("style","color: #3d35df !important;");
-                var btn = document.createElement("button");
-                btn.setAttribute("class","a-button-input attach-dss-atc");
-                btn.setAttribute("href","#addToCart");
-                document.getElementsByClassName("a-section a-spacing-small a-text-center")[0].appendChild(btn);
-
-            }
-        }
+        const btn = document.createElement("button");
+        btn.setAttribute("href","#addToCart");
+        btn.setAttribute("style","background-color: #BF8F00 !important; border-radius: 10px; border: 0px; padding: 10px;");
+        btn.setAttribute("id","addToCart");
+        btn.innerText = "Jimpo26 | Cart\nClick here to add the item to the cart";
+        item.prepend(btn);
     }
 }
 btn()
-
-//to run this script on bookmarks, copy the script from pastebin https://pastebin.com/raw/SNVHBhEg and paste it in the bookmark
